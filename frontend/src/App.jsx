0c7ba9f93d13643a5121872f2634ba2670d49e3d
sai sarayu <sarayu2926@gmail.com>
2025-01-31 17:55:55 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 import {Routes,Route} from 'react-router-dom'
+import { LoginPage } from './routes/routes'
 function App() {
   
 
   return (
    <>
    <Routes>
-    <route path='/login' element={<LoginPage/>}/>
+    <Route path='/login' element={<LoginPage/>}/>
    </Routes>
    </>
   )
 }
+export default App
---
+++ frontend/src/routes/routes.jsx
@@ -0,0 +1,3 @@
+import LoginPage from "../page/login/LoginPage"
+
+export {LoginPage}
